a0b22704d73c8fbc5ed4297f009fbcf368e97c5c
ghsantos <ghsantos933@gmail.com>
2020-04-02 14:29:21 -0300

    fix index.js import

--- index.js
+++ index.js
@@ -1,60 +1,9 @@
-//
-//  react-native-orientation-locker
-//
-//
-//  Created by Wonday on 17/5/12.
-//  Copyright (c) wonday.org All rights reserved.
-//
+import { Platform } from "react-native";
+import OrientationIos from "./index.ios.js";
+import OrientationAndroid from "./index.android.js";
 
-"use strict";
+const Orientation = Platform.OS === "ios"
+  ? OrientationIos
+  : OrientationAndroid;
 
-export default class Orientation {
-  static getOrientation = cb => {
-    cb("UNKNOWN");
-  };
-
-  static getDeviceOrientation = cb => {
-    cb("UNKNOWN");
-  };
-
-  static isLocked = () => {
-    return false;
-  };
-
-  static lockToPortrait = () => {};
-
-  static lockToPortraitUpsideDown = () => {};
-
-  static lockToLandscape = () => {};
-
-  static lockToLandscapeRight = () => {};
-
-  static lockToLandscapeLeft = () => {};
-
-  // OrientationMaskAllButUpsideDown
-  static lockToAllOrientationsButUpsideDown = () => {};
-
-  static unlockAllOrientations = () => {};
-
-  static addOrientationListener = cb => {};
-
-  static removeOrientationListener = cb => {};
-
-  static addDeviceOrientationListener = cb => {};
-
-  static removeDeviceOrientationListener = cb => {};
-
-  static addLockListener = cb => {};
-
-  static removeLockListener = cb => {};
-
-  static removeAllListeners = () => {};
-
-  static getInitialOrientation = () => {
-    return "UNKNOWN";
-  };
-
-  static getAutoRotateState = cb => {
-    cb(true);
-  };
-}
+export default Orientation;
